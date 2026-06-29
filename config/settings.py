9b3f743e8b10f8066fde60ae66cabce1c3595320
andreaslarssamils <larssamils.andreas@gmail.com>
2026-06-29 19:44:53 +0200

    Add database configuration support and create .gitkeep for static directory

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
 from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,10 +112,10 @@ ACCOUNT_LOGOUT_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        env.str("DATABASE_URL", default="")
+        or f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
